3b35a1b11a38999c2686e2479fc30e5933a336da
Deeshai Escoffery <deeshaiesc@fb.com>
2017-07-12 02:58:54 -0700

    bottom navigation bar

--- app/src/main/java/com/example/gabbygiordano/marketplace/HomeActivity.java
+++ app/src/main/java/com/example/gabbygiordano/marketplace/HomeActivity.java
@@ -1,7 +1,13 @@
 package com.example.gabbygiordano.marketplace;
 
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +15,42 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        final BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottomNavigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
+        {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item)
+            {
+
+                switch (item.getItemId())
+                {
+                    case R.id.action_home:
+                        Toast.makeText(HomeActivity.this, "Home Tab Selected", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.action_search:
+                        Toast.makeText(HomeActivity.this, "Search Tab Selected", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.action_add:
+                        Toast.makeText(HomeActivity.this, "Add Tab Selected", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.action_notifications:
+                        Toast.makeText(HomeActivity.this, "Notifications Tab Selected", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.action_profile:
+                        Toast.makeText(HomeActivity.this, "Profile Tab Selected", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return true;
+            }
+        });
+
     }
 }
