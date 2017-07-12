7a9484a47c6f8be658e09889f3539731fe5e9985
tanvigupta17 <tanvigupta0217@gmail.com>
2017-07-12 10:50:13 -0700

    Added fragments to home activity

---
+++ app/src/main/java/com/example/gabbygiordano/marketplace/fragments/AllTimelineFragment.java
@@ -0,0 +1,17 @@
+package com.example.gabbygiordano.marketplace.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by tanvigupta on 7/12/17.
+ */
+
+public class AllTimelineFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
