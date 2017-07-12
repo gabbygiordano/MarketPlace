9417e1fb5f33a9467fb466c7afe85dabec2c645b
Deeshai Escoffery <deeshaiesc@fb.com>
2017-07-12 12:43:50 -0700

    added use of camera services

--- app/src/main/java/com/example/gabbygiordano/marketplace/AddItemActivity.java
+++ app/src/main/java/com/example/gabbygiordano/marketplace/AddItemActivity.java
@@ -1,14 +1,22 @@
 package com.example.gabbygiordano.marketplace;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RatingBar;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class AddItemActivity extends AppCompatActivity {
+
+    private static final int ACTIVITY_START_CAMERA = 0;
 
     public EditText etItemName;
     public EditText etItemDescription;
@@ -16,6 +24,9 @@ public class AddItemActivity extends AppCompatActivity {
     public RatingBar rbCondition;
     public ImageButton ibAddImage;
     public ImageButton ibPostItem;
+    public ImageView imageLocation;
+
+    final ArrayList<ImageView> addImage = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +39,31 @@ public class AddItemActivity extends AppCompatActivity {
         rbCondition = (RatingBar) findViewById(R.id.rbCondition);
         ibAddImage = (ImageButton) findViewById(R.id.ibAddImage);
         ibPostItem = (ImageButton) findViewById(R.id.ibPostItem);
-
+        imageLocation= (ImageView) findViewById(R.id.ivItemPhoto);
     }
 
     public void onPostSuccess(View view){
         Intent i = new Intent(this, HomeActivity.class);
         startActivity(i);
+    }
+
+
+    public void takeItemPhoto(View view)
+    {
+        Intent intent = new Intent();
+        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, ACTIVITY_START_CAMERA);
+    }
+
+    protected void onActivityResult(int requestcode, int resultCode, Intent data)
+    {
+        if(requestcode == ACTIVITY_START_CAMERA && resultCode == RESULT_OK)
+        {
+            //Toast.makeText(this, "picture was taken", Toast.LENGTH_SHORT).show();
+            Bundle extras = data.getExtras();
+            Bitmap photoCaptured = (Bitmap) extras.get("data");
+            imageLocation.setImageBitmap(photoCaptured);
+
+        }
     }
 }
