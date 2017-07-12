a937252a302af36f6c584bb7d114163502905bcd
Deeshai Escoffery <deeshaiesc@fb.com>
2017-07-11 17:36:22 -0700

    set up personal activity

--- app/src/main/java/com/example/gabbygiordano/marketplace/SignUpActivity.java
+++ app/src/main/java/com/example/gabbygiordano/marketplace/SignUpActivity.java
@@ -17,7 +17,7 @@ public class SignUpActivity extends AppCompatActivity {
         Spinner spinner = (Spinner) findViewById(R.id.schoolOptions);
 // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.planets_array, android.R.layout.simple_spinner_item);
+                R.array.schools_array, android.R.layout.simple_spinner_item);
 // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
