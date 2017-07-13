ae203978ba49f6df5422f8e90c5aa44c3c39b6b5
tanvigupta17 <tanvigupta0217@gmail.com>
2017-07-13 12:05:43 -0700

    Stored user signup data to server, implemented automatic logins

--- app/src/main/java/com/example/gabbygiordano/marketplace/LoginActivity.java
+++ app/src/main/java/com/example/gabbygiordano/marketplace/LoginActivity.java
@@ -8,6 +8,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
 public class LoginActivity extends AppCompatActivity {
 
     Button btLogIn;
@@ -24,17 +28,23 @@ public class LoginActivity extends AppCompatActivity {
         etUserName = (EditText) findViewById(R.id.etUserName);
         etPassword = (EditText) findViewById(R.id.etPassword);
         tvForgot = (TextView) findViewById(R.id.tvForgot);
-
-
-
-
-
     }
 
     public void onLoginSuccess(View view){
-        // if username and password are accepted, start new activity
+        String username = etUserName.getText().toString();
+        String password = etPassword.getText().toString();
 
-        Intent i = new Intent(this, HomeActivity.class);
-        startActivity(i);
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (user != null) {
+                    // Hooray! The user is logged in.
+                    Intent i = new Intent(getApplicationContext(), HomeActivity.class);
+                    startActivity(i);
+                } else {
+                    // Signup failed. Look at the ParseException to see what happened.
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/gabbygiordano/marketplace/User.java
+++ app/src/main/java/com/example/gabbygiordano/marketplace/User.java
@@ -14,12 +14,13 @@ public class User {
     public String email;
     public String password;
     public String college;
-    public String phone;
+    public long phone;
+    public String contactMethod;
 
     public User() {
     }
 
-    public static User fromInput(String name, String username, String email, String password, String college, String phone) {
+    public static User fromInput(String name, String username, String email, String password, String college, long phone, String contact) {
         User user = new User();
 
         user.name = name;
@@ -28,6 +29,7 @@ public class User {
         user.password = password;
         user.college = college;
         user.phone = phone;
+        user.contactMethod = contact;
 
         return user;
     }
