5bd141fb599932448a6421450df7b1cbd6c464cb
Gabby Giordano <gabby.giordano@gmail.com>
2017-07-18 13:49:06 -0700

    added ability to populate profile timeline with items that user is selling and other little things

--- app/src/main/java/com/example/gabbygiordano/marketplace/ProfileActivity.java
+++ app/src/main/java/com/example/gabbygiordano/marketplace/ProfileActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,7 +16,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.gabbygiordano.marketplace.ItemAdapter.getContext;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -23,11 +34,31 @@ public class ProfileActivity extends AppCompatActivity {
     TextView tvCollege;
     TextView tvPhone;
     ImageButton ibLogOut;
+    RecyclerView rvProfileItems;
+
+    ArrayList<Item> items;
+    ItemAdapter itemAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        // perform find view by id lookups
+        rvProfileItems = (RecyclerView) findViewById(R.id.rvProfileItems);
+
+        // initialize arraylist
+        items = new ArrayList<>();
+
+        //construct the adapter from the array list
+        itemAdapter = new ItemAdapter(items, getContext());
+
+        // RecyclerView setup (layout manager, use adapter)
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        rvProfileItems.setLayoutManager(linearLayoutManager);
+        rvProfileItems.setAdapter(itemAdapter);
+        rvProfileItems.setHasFixedSize(true);
+
 
         // perform find view by id lookups
         ivProfileImage = (ImageView) findViewById(R.id.ivProfileImage);
@@ -107,6 +138,34 @@ public class ProfileActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        populateProfileTimeline();
+    }
+
+    public void populateProfileTimeline(){
+        ParseQuery<Item> query = ParseQuery.getQuery(Item.class);
+        query.whereEqualTo("owner", ParseUser.getCurrentUser());
+        query.include("owner");
+        query.setLimit(200);
+        query.orderByDescending("_created_at");
+        query.findInBackground(new FindCallback<Item>() {
+            public void done(List<Item> itemsList, ParseException e) {
+                if (e == null) {
+                    Log.d("items", "Retrieved " + itemsList.size() + " items");
+                    addItems(itemsList);
+                } else {
+                    Log.d("items", "Error: " + e.getMessage());
+                }
+            }
+        });
+    }
+
+    public void addItems(List<Item> list){
+        for(int i=0; i< list.size(); i++){
+            items.add(list.get(i));
+            itemAdapter.notifyItemInserted(items.size()-1);
+        }
+
     }
 
 
