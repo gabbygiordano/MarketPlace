bd73d9054b4354c36187752a2b16c74ade4efb7c
Gabby Giordano <gabby.giordano@gmail.com>
2017-07-20 11:23:18 -0700

    implemented searchbar functionality

--- app/src/main/java/com/example/gabbygiordano/marketplace/SearchActivity.java
+++ app/src/main/java/com/example/gabbygiordano/marketplace/SearchActivity.java
@@ -4,22 +4,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.SearchView.OnQueryTextListener;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
 
-    RecyclerView rvSearch;
+    RecyclerView rvItems;
     ItemAdapter itemAdapter;
     MarketPlaceClient client;
-    ArrayList<Item> aitems;
-
+    ArrayList<Item> items;
+    SearchView searchView;
 
 
     @Override
@@ -27,19 +38,17 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        rvSearch = (RecyclerView) findViewById(R.id.rvSearch);
-        aitems = new ArrayList<>();
+        rvItems = (RecyclerView) findViewById(R.id.rvItems);
+        items = new ArrayList<>();
 
         // initialize adapter
-        itemAdapter = new ItemAdapter(aitems, this);
+        itemAdapter = new ItemAdapter(items, this);
 
         // attatch adapter to recycler view
-        rvSearch.setAdapter(itemAdapter);
+        rvItems.setAdapter(itemAdapter);
 
         // set layout manager to position items
-        rvSearch.setLayoutManager(new LinearLayoutManager(this));
-
-
+        rvItems.setLayoutManager(new LinearLayoutManager(this));
 
 
         BottomNavigationView bottomNavigationView;
@@ -51,13 +60,10 @@ public class SearchActivity extends AppCompatActivity {
         MenuItem menuitem = menu.getItem(1);
         menuitem.setChecked(true);
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
-        {
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item)
-            {
-                switch (item.getItemId())
-                {
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
                     case R.id.action_home:
                         //Toast.makeText(HomeActivity.this, "Home Tab Selected", Toast.LENGTH_SHORT).show();
                         Intent i_home = new Intent(SearchActivity.this, HomeActivity.class);
@@ -92,6 +98,76 @@ public class SearchActivity extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_search, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchView.clearFocus();
+                items.clear();
+            }
+        });
+        searchView.setOnQueryTextListener(new OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String text) {
+                // perform query here
+                ParseQuery<Item> query = ParseQuery.getQuery(Item.class);
+                query.include("owner");
+                query.whereContains("item_name", text);
+                query.orderByDescending("_created_at");
+                query.findInBackground(new FindCallback<Item>() {
+                    public void done(List<Item> itemsList, ParseException e) {
+                        if (e == null) {
+                            // item was found
+                            if (itemsList != null && !itemsList.isEmpty()) {
+                                addItems(itemsList);
+                            }
+                        } else {
+                            Log.e("ItemsListFragment", e.getMessage());
+                        }
+                    }
+                });
+
+
+                // workaround to avoid issues with some emulators and keyboard devices firing twice if a keyboard enter is used
+                // see https://code.google.com/p/android/issues/detail?id=24599
+                searchView.clearFocus();
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                items.clear();
+                return false;
+            }
+
+            // TODO: implement so the seach query changes as you type
+            // @Override
+            // public boolean onQueryTextChange(String newText) {
+             //   fetchBooks(newText);
+            //    return false;
+            //}
+
+        });
+        return super.onCreateOptionsMenu(menu);
+
+
+    }
+
+    public void addItems(List<Item> list){
+        for(int i=0; i< list.size(); i++){
+            items.add(list.get(i));
+            itemAdapter.notifyItemInserted(items.size()-1);
+        }
+
+    }
+
 
 
 }
