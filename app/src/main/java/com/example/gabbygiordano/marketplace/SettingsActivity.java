ea5d7bd3462f824a0bda2492f9646bdd0f45b6e7
tanvigupta17 <tanvigupta0217@gmail.com>
2017-08-01 11:22:26 -0700

    Settings UI+updates, added item type icons

--- app/src/main/java/com/example/gabbygiordano/marketplace/SettingsActivity.java
+++ app/src/main/java/com/example/gabbygiordano/marketplace/SettingsActivity.java
@@ -1,10 +1,10 @@
 package com.example.gabbygiordano.marketplace;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -17,19 +17,25 @@ public class SettingsActivity extends AppCompatActivity {
     TextView tvName;
     TextView tvPhone;
     TextView tvEmail;
+    TextView tvUsername;
     TextView tvUploadProf;
     ImageButton ibUploadProf;
 
     EditText etName;
     EditText etPhone;
     EditText etEmail;
+    EditText etUsername;
 
-    Button btSaveChanges;
+    ImageButton btSaveChanges;
     ImageButton ibLogout;
 
-    Boolean changedName;
-    Boolean changedPhone;
-    Boolean changedEmail;
+    boolean changedName;
+    boolean changedPhone;
+    boolean changedEmail;
+    boolean changedUsername;
+    boolean changedPassword;
+
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,28 +43,35 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         getSupportActionBar().setTitle("Settings");
 
+        context = this;
+
         tvName = (TextView) findViewById(R.id.tvName);
         tvPhone = (TextView) findViewById(R.id.tvPhone);
         tvEmail = (TextView) findViewById(R.id.tvEmail);
+        tvUsername = (TextView) findViewById(R.id.tvUsername);
         tvUploadProf = (TextView) findViewById(R.id.tvUploadProf);
         ibUploadProf = (ImageButton) findViewById(R.id.ibUploadProf);
 
         etName = (EditText) findViewById(R.id.etName);
         etPhone = (EditText) findViewById(R.id.etPhone);
         etEmail = (EditText) findViewById(R.id.etEmail);
+        etUsername = (EditText) findViewById(R.id.etUsername);
 
-        btSaveChanges = (Button) findViewById(R.id.btSaveChanges);
+        btSaveChanges = (ImageButton) findViewById(R.id.btSaveChanges);
         ibLogout = (ImageButton) findViewById(R.id.ibLogOut);
 
         changedName = true;
         changedPhone = true;
         changedEmail = true;
+        changedUsername = true;
+        changedPassword = true;
 
         final ParseUser user = ParseUser.getCurrentUser();
         if (user != null) {
             etName.setText(user.getString("name"), TextView.BufferType.EDITABLE);
             etPhone.setText(String.valueOf(user.getLong("phone")), TextView.BufferType.EDITABLE);
             etEmail.setText(user.getString("email"), TextView.BufferType.EDITABLE);
+            etUsername.setText(user.getUsername(), TextView.BufferType.EDITABLE);
         }
 
         // log out if power button is clicked
